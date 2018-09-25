152921c37995b3e55b5c09c7d672af5c447481fb
Martin <martinstojchev@hotmail.com>
2018-09-25 15:41:42 +0200

    Initial Commit

---
+++ Locations/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Locations
+//
+//  Created by Martin on 9/25/18.
+//  Copyright © 2018 Martin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
